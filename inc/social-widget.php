066b4a4346b59d48ff5d745e82f863e51de17856
Bonnie Martin <bonjmartn@gmail.com>
2016-08-18 13:07:13 -0700

    Add files via upload

--- inc/social-widget.php
+++ inc/social-widget.php
@@ -18,8 +18,8 @@ class homepage_social_widget extends WP_Widget {
   function __construct() {
     parent::__construct(
       'homepage_social_widget', // Base ID
-      __( 'Homepage Social Icons', 'color-block-free' ), // Name
-      array( 'description' => __( 'Drag me to the Homepage Social Icons area', 'color-block-free' ), ) // Args
+      __( 'Homepage Social Icons', 'color-block' ), // Name
+      array( 'description' => __( 'Drag me to the Homepage Social Icons area', 'color-block' ), ) // Args
     );
   }
 
@@ -37,15 +37,15 @@ class homepage_social_widget extends WP_Widget {
       echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
     }
  
-    $facebook = $instance['facebook'];
-    $twitter = $instance['twitter'];
-    $pinterest = $instance['pinterest'];
-    $instagram = $instance['instagram'];
-    $googleplus = $instance['googleplus'];
-    $yelp = $instance['yelp'];
-    $youtube = $instance['youtube'];
-    $linkedin = $instance['linkedin'];
-
+    $facebook = esc_url( $instance['facebook'] );
+    $twitter = esc_url( $instance['twitter'] );
+    $pinterest = esc_url( $instance['pinterest'] );
+    $instagram = esc_url( $instance['instagram'] );
+    $googleplus = esc_url( $instance['googleplus'] );
+    $yelp = esc_url( $instance['yelp'] );
+    $youtube = esc_url( $instance['youtube'] );
+    $linkedin = esc_url( $instance['linkedin'] );
+    
     if ( ! empty( $instance['facebook'] ) ) {
       echo sprintf( '<a href="' . $facebook . '"><i class="fa fa-facebook-square"></i></a>');
     }
@@ -89,68 +89,68 @@ class homepage_social_widget extends WP_Widget {
    * @param array $instance Previously saved values from database.
    */
   public function form( $instance ) {
-    $title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Follow Us', 'color-block-free' );
-    $facebook = ! empty( $instance['facebook'] ) ? $instance['facebook'] : __( 'Facebook URL', 'color-block-free' );
-    $twitter = ! empty( $instance['twitter'] ) ? $instance['twitter'] : __( 'Twitter URL', 'color-block-free' );
-    $pinterest = ! empty( $instance['pinterest'] ) ? $instance['pinterest'] : __( 'Pinterest URL', 'color-block-free' );
-    $instagram = ! empty( $instance['instagram'] ) ? $instance['instagram'] : __( 'Instagram URL', 'color-block-free' );
-    $googleplus = ! empty( $instance['googleplus'] ) ? $instance['googleplus'] : __( 'Google Plus URL', 'color-block-free' );
-    $yelp = ! empty( $instance['yelp'] ) ? $instance['yelp'] : __( 'Yelp URL', 'color-block-free' );
-    $youtube = ! empty( $instance['youtube'] ) ? $instance['youtube'] : __( 'YouTube URL', 'color-block-free' );
-    $linkedin = ! empty( $instance['linkedin'] ) ? $instance['linkedin'] : __( 'LinkedIn URL', 'color-block-free' );
+    $title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Follow Us', 'color-block' );
+    $facebook = ! empty( $instance['facebook'] ) ? $instance['facebook'] : __( '', 'color-block' );
+    $twitter = ! empty( $instance['twitter'] ) ? $instance['twitter'] : __( '', 'color-block' );
+    $pinterest = ! empty( $instance['pinterest'] ) ? $instance['pinterest'] : __( '', 'color-block' );
+    $instagram = ! empty( $instance['instagram'] ) ? $instance['instagram'] : __( '', 'color-block' );
+    $googleplus = ! empty( $instance['googleplus'] ) ? $instance['googleplus'] : __( '', 'color-block' );
+    $yelp = ! empty( $instance['yelp'] ) ? $instance['yelp'] : __( '', '' );
+    $youtube = ! empty( $instance['youtube'] ) ? $instance['youtube'] : __( '', 'color-block' );
+    $linkedin = ! empty( $instance['linkedin'] ) ? $instance['linkedin'] : __( '', 'color-block' );
     ?>
 
 
     <p>
-    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'color-block-free' ); ?></label> 
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'color-block' ); ?></label> 
     <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" 
     value="<?php echo esc_attr( $title ); ?>">
     </p>
 
     <p>
-    <label for="<?php echo $this->get_field_id('facebook_field'); ?>"><?php _e('Enter the URL for your Facebook page', 'color-block-free'); ?></label>
+    <label for="<?php echo $this->get_field_id('facebook_field'); ?>"><?php _e('Enter the URL for your Facebook page', 'color-block'); ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id('facebook_field'); ?>" name="<?php echo $this->get_field_name('facebook_field'); ?>" type="text" 
     value="<?php echo esc_attr( $facebook ); ?>" />
     </p>
 
     <p>
-    <label for="<?php echo $this->get_field_id('twitter_field'); ?>"><?php _e('Enter the URL for your Twitter profile', 'color-block-free'); ?></label>
+    <label for="<?php echo $this->get_field_id('twitter_field'); ?>"><?php _e('Enter the URL for your Twitter profile', 'color-block'); ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id('twitter_field'); ?>" name="<?php echo $this->get_field_name('twitter_field'); ?>" type="text" 
     value="<?php echo esc_attr( $twitter ); ?>" />
     </p>
 
     <p>
-    <label for="<?php echo $this->get_field_id('pinterest_field'); ?>"><?php _e('Enter the URL for your Pinterest page', 'color-block-free'); ?></label>
+    <label for="<?php echo $this->get_field_id('pinterest_field'); ?>"><?php _e('Enter the URL for your Pinterest page', 'color-block'); ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id('pinterest_field'); ?>" name="<?php echo $this->get_field_name('pinterest_field'); ?>" type="text" 
     value="<?php echo esc_attr( $pinterest ); ?>" />
     </p>
 
     <p>
-    <label for="<?php echo $this->get_field_id('instagram_field'); ?>"><?php _e('Enter the URL for your Instagram profile', 'color-block-free'); ?></label>
+    <label for="<?php echo $this->get_field_id('instagram_field'); ?>"><?php _e('Enter the URL for your Instagram profile', 'color-block'); ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id('instagram_field'); ?>" name="<?php echo $this->get_field_name('instagram_field'); ?>" type="text" 
     value="<?php echo esc_attr( $instagram ); ?>" />
     </p>
 
     <p>
-    <label for="<?php echo $this->get_field_id('googleplus_field'); ?>"><?php _e('Enter the URL for your Google Plus profile', 'color-block-free'); ?></label>
+    <label for="<?php echo $this->get_field_id('googleplus_field'); ?>"><?php _e('Enter the URL for your Google Plus profile', 'color-block'); ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id('googleplus_field'); ?>" name="<?php echo $this->get_field_name('googleplus_field'); ?>" type="text" 
     value="<?php echo esc_attr( $googleplus ); ?>" />
     </p>
 
     <p>
-    <label for="<?php echo $this->get_field_id('yelp_field'); ?>"><?php _e('Enter the URL for your Yelp page', 'color-block-free'); ?></label>
+    <label for="<?php echo $this->get_field_id('yelp_field'); ?>"><?php _e('Enter the URL for your Yelp page', 'color-block'); ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id('yelp_field'); ?>" name="<?php echo $this->get_field_name('yelp_field'); ?>" type="text" 
     value="<?php echo esc_attr( $yelp ); ?>" />
     </p>
 
     <p>
-    <label for="<?php echo $this->get_field_id('youtube_field'); ?>"><?php _e('Enter the URL for your YouTube page', 'color-block-free'); ?></label>
+    <label for="<?php echo $this->get_field_id('youtube_field'); ?>"><?php _e('Enter the URL for your YouTube page', 'color-block'); ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id('youtube_field'); ?>" name="<?php echo $this->get_field_name('youtube_field'); ?>" type="text" 
     value="<?php echo esc_attr( $youtube ); ?>" />
     </p>
 
     <p>
-    <label for="<?php echo $this->get_field_id('linkedin_field'); ?>"><?php _e('Enter the URL for your LinkedIn profile', 'color-block-free'); ?></label>
+    <label for="<?php echo $this->get_field_id('linkedin_field'); ?>"><?php _e('Enter the URL for your LinkedIn profile', 'color-block'); ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id('linkedin_field'); ?>" name="<?php echo $this->get_field_name('linkedin_field'); ?>" type="text" 
     value="<?php echo esc_attr( $linkedin ); ?>" />
     </p>
